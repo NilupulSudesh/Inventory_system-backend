069be2fc5c1edd6311a8e48b0c1ef612b2f2af6a
NilupulSudesh <nilupulsudesh@gmail.com>
2026-04-25 14:19:22 +0530

    initial commit

---
+++ src/main/java/edu/Inventory_system/Odoo/OdooApplication.java
@@ -0,0 +1,13 @@
+package edu.Inventory_system.Odoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/Inventory_system/Odoo/OdooApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.Inventory_system.Odoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

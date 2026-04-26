2b5d4e0272c366557a59e2d1802106aca938136c
NilupulSudesh <nilupulsudesh@gmail.com>
2026-04-26 14:27:57 +0530

    create security

---
+++ src/main/java/edu/Inventory_system/Odoo/security/JwtUtils.java
@@ -0,0 +1,66 @@
+package edu.Inventory_system.Odoo.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+@Slf4j
+public class JwtUtils {
+
+    private static final long EXPIRATION_TIME_IN_MILLISEC = 1000L * 60L * 60L * 24L * 30L * 6L; //expires in 6 months in milleces
+    private SecretKey key;
+
+    @Value("${secreteJwtString}")
+    private String secreteJwtString;
+
+
+    @PostConstruct
+    private void init() {
+        byte[] keyByte = secreteJwtString.getBytes(StandardCharsets.UTF_8);
+        this.key = new SecretKeySpec(keyByte, "HmacSHA256");
+    }
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME_IN_MILLISEC))
+                .signWith(key)
+                .compact();
+    }
+
+    public String getUsernameFromToken(String token) {
+        return extractClaims(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaims(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claimsResolver.apply(claims);
+    }
+
+    public boolean isTokeValid(String token, UserDetails userDetails) {
+        final String username = getUsernameFromToken(token);
+        return (username.equals(userDetails.getUsername()) && !isTokeExpired(token));
+    }
+
+    private boolean isTokeExpired(String token) {
+        return extractClaims(token, Claims::getExpiration).before(new Date());
+    }
+
+
+}
